5b93842a1f37ca36bb390388032ee06b54ddef56
Davpad <davidpadillavillen@gmail.com>
2025-02-04 13:55:22 +0100

    Feat: changes in migrations and layout to make it work

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -29,16 +29,17 @@ import { EditarTienda } from "./pages/editartienda";
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+    // const basename = process.env.BASENAME || "";
 
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
         <div>
-            <BrowserRouter basename={basename}>
+            <BrowserRouter basename="/app">
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
+                        
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Nosotros />} path="/nosotros" />
