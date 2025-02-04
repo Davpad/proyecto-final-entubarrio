79056ee25d41196dec9f699a057755c8ce2e60f4
Davpad <davidpadillavillen@gmail.com>
2025-02-04 19:47:03 +0100

    Feat: updating layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -32,6 +32,7 @@ const Layout = () => {
 
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
+    
     return (
         <div>
             <BrowserRouter basename="/app">
