e5d3bb72ed67fd39ba7d181d60741a17b1d95dd1
Davpad <davidpadillavillen@gmail.com>
2025-02-12 19:49:20 +0100

    Feat: finish cloudinary component

--- src/front/js/component/cloudinary.jsx
+++ src/front/js/component/cloudinary.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 
 export const Cloudinary = () => {
 
-    const [loading, setLoading] = useState(false) 
+    const [ image, setImage ] = useState('');
+    const [ loading, setLoading ] = useState(false) 
 
     const uploadImage = async (e)=>{
         const files = e.target.files
@@ -12,6 +13,22 @@ export const Cloudinary = () => {
         data.append('upload_preset', preset_name)
 
         setLoading(true)
+
+        try{
+            const response = await fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, {
+                method: 'POST',
+                body: data
+            });
+
+            const file = await response.json()
+            setImage(file.secure_url);
+            setLoading(false);
+            //await actions.sendPhoto(file.secure_url)
+        } catch (error) {
+            console.error('Error uploading image:', error);
+            setLoading(false);
+        }
+        
     }
 
     return(
